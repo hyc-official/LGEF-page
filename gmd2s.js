be3cbda3763c57f6ed0d57c305183ac8b9b58062
Yangcheng He <107044023+hyc1230@users.noreply.github.com>
2023-07-23 10:54:51 +0800

    upd

--- gmd2s.js
+++ gmd2s.js
@@ -3,6 +3,7 @@ var dir = "/md";
 
 function load()
 {
+    document.getElementById("content").innerHTML = "<center>加载中，请稍候...</center>";
     let doc = dir + window.location.pathname;
     if (doc[doc.length - 1] == "/") {
         doc += "index.md";
@@ -25,7 +26,7 @@ function load()
             document.getElementById("content").innerHTML = marked.parse(result);
         }
         catch (err) {
-            document.getElementById("content").innerHTML = `<center><p style="color:#f00;font-size:1.25em;"><b>加载失败！请检查网址拼写或网络连接！</b><br><br>请求文件：${window.location.protocol}//${window.location.host}${doc}</p></center>`;
+            document.getElementById("content").innerHTML = `<center><p style="color:#f00;font-size:1.25em;"><b>加载失败！请检查网址拼写或网络连接！</b><br><br>请求文件：${window.location.protocol}//${window.location.host}${doc}<br><br>错误：${err}</p></center>`;
         }
     });
 }
