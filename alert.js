f516462e10a98193801cca2553086d45001ac83a
hyc1230 <i@heyc.eu.org>
2023-11-01 00:18:13 +0000

    upd

--- alert.js
+++ alert.js
@@ -1,6 +1,6 @@
 var ver = "0.1.3";
 var alert = `<p><i class="fa fa-flag-o"></i> 公告：LGEF 正式发布 0.1.0 版本！<br>
-<i class="fa fa-code"></i> 当前最新版本：<span style="color: #e67e22">${ver} LRV</span><br>
+<i class="fa fa-code"></i> 当前最新版本：<span style="color: #e67e22" id="version">正在获取中...</span><br>
 <i class="fa fa-envelope-o"></i> 访问量统计：<img src="https://badges.toozhao.com/badges/01H5XRS8A83AAJHPPR1WMZRH07/blue.svg" style="height: 1em;" /></p>`;
 var bar = `&nbsp;&nbsp;&nbsp;
 <span class="BarElement"><i class="fa fa-list"></i> 导航</span>
@@ -15,8 +15,17 @@ var foot = `<center>
     <p>Developed by <a href="https://blog.heyc.eu.org" target="_blank">Heyc</a>, <a href="https://github.com/hyc-official/LGEF-page" target="_blank">GitHub repo</a></p>
     <br>
 </center>`;
+async function set_version() {
+    fetch("https://api.kgithub.com/repos/hyc-official/LuoguExtendFeed/releases/latest")
+      .then((response) => response.json())
+      .then((data) => {
+          console.log(data);
+          document.getElementById("version").innerText = data.tag_name + " (由 " + data.author.login + " 发布)";
+      });
+}
 function set_alert() {
     document.getElementById("alert").innerHTML = `<p>${alert}</p>`;
     document.getElementById("bar").innerHTML = bar;
     document.getElementById("foot").innerHTML = foot;
+    set_version();
 }
